026d3091433bd83bd1bcb5b8e0bb1b1bc7763865
Lin Zhang <marshluca@gmail.com>
2013-09-02 21:10:31 +0800

    customize migration for agent

--- db/migrate/20130902125930_create_agents.rb
+++ db/migrate/20130902125930_create_agents.rb
@@ -1,9 +1,9 @@
 class CreateAgents < ActiveRecord::Migration
   def change
     create_table :agents do |t|
-      t.string :agent_no
-      t.string :first_name
-      t.string :last_name
+      t.string :agent_no,   limit: 50, null: false, unique: true
+      t.string :first_name, limit: 50 
+      t.string :last_name,  limit: 50
 
       t.timestamps
     end
