40c6a5316ce87b9577f081db37a4204f068e979d
Lin Zhang <marshluca@gmail.com>
2013-09-02 21:48:29 +0800

    Add a simple test for agent's default address

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,3 +1,5 @@
 class Agent < ActiveRecord::Base
   has_many :addresses
+
+  attr_accessor :default_address
 end
--- spec/models/agent_spec.rb
+++ spec/models/agent_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Agent do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should allow to set a default address" do
+    @agent = FactoryGirl.create(:agent)
+    @agent.default_address = "Agent's default address"
+    @agent.default_address == "Agent's default address"
+  end
 end
