b4bbee54dbc2738d6f246a38abdce8da7661e9c1
Lin Zhang <marshluca@gmail.com>
2013-09-02 21:00:03 +0800

    bundle exec rails g model agent agent_no first_name last_name

---
+++ spec/factories/agents.rb
@@ -0,0 +1,9 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :agent do
+    agent_no "MyString"
+    first_name "MyString"
+    last_name "MyString"
+  end
+end
