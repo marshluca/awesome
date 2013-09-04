435a5840186a55e4bb60be7f21d22bda11109729
Lin Zhang <marshluca@gmail.com>
2013-09-04 09:27:21 +0800

    Add scenario: user add set default address for agent

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -11,4 +11,13 @@ feature "Addresses" do
     visit agents_path
     page.should have_content('Default address')
   end
+
+  scenario "user can set default address" do
+    visit agents_path
+    click_link 'Address'
+    current_url.should == agent_addresses_url(@agent)
+    click_link 'Mark default'
+    current_url.should == agents_url
+    page.should have_content("Address was successfully marked as default.")
+  end
 end
