06b045963a10389ef9ce16b91088af8455cb698a
Lin Zhang <marshluca@gmail.com>
2013-09-04 10:08:17 +0800

    remove rspec warnnings

--- spec/models/agent_spec.rb
+++ spec/models/agent_spec.rb
@@ -1,20 +1,21 @@
 require 'spec_helper'
 
 describe Agent do
-  it "should have a default address" do
+  before do
     @agent = FactoryGirl.create(:agent)
+  end
+
+  it "should have a default address" do
     @agent.should respond_to(:default_address)
   end
 
   it "should allow to set a default address" do
-    @agent = FactoryGirl.create(:agent)
     @address = FactoryGirl.create(:address, agent_id: @agent.id)
     @agent.default_address = @address
     @agent.default_address.should eql(@address.address1)
   end
 
   it "should have a full name" do
-    @agent = FactoryGirl.create(:agent)
     @agent.full_name.should eql("#{@agent.first_name} #{@agent.last_name}")
   end
 end
